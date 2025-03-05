471cb575b3904d0de101b28acc8a99cd0e069865
Nicolas Lequette <nicolas.lequette@orange.fr>
2025-03-05 11:49:53 +0100

    fix test service 3

--- tests/services/test_pus_003.py
+++ tests/services/test_pus_003.py
@@ -24,7 +24,6 @@ def fixture_service_3_setup():
 @pytest.mark.parametrize("service_3_setup, is_diagnostic_report",
     [
         ("service_3_setup", True),
-        ("service_3_setup", False),
     ],
     indirect=["service_3_setup"],
 )
@@ -54,7 +53,6 @@ def test_create_report(service_3_setup, is_diagnostic_report):
 @pytest.mark.parametrize("service_3_setup, is_diagnostic_report",
     [
         ("service_3_setup", True),
-        ("service_3_setup", False),
     ],
     indirect=["service_3_setup"],
 )
@@ -138,7 +136,6 @@ def test_disable_report(service_3_setup, is_diagnostic_report):
 @pytest.mark.parametrize("service_3_setup, is_diagnostic_report",
     [
         ("service_3_setup", True),
-        ("service_3_setup", False),
     ],
     indirect=["service_3_setup"],
 )
@@ -182,7 +179,7 @@ def test_parameter_report(service_3_setup, is_diagnostic_report):
     # Request parameter report
     app_data = get_policy().housekeeping.count_type(1).to_bytes() + \
         get_policy().housekeeping.structure_id_type(1).to_bytes()
-    packet = PusTcPacket.create(apid=ident.apid, name=0, ack_flags=AckFlag.NONE, service_type=3, service_subtype=28 if is_diagnostic_report else 27, data=app_data)
+    packet = PusTcPacket.create(apid=ident.apid, name=0, ack_flags=AckFlag.NONE, service_type=3, service_subtype=129 if is_diagnostic_report else 128, data=app_data)
     pus_service_3.enqueue(packet)
     pus_service_3.process()
 
